20aae33a2354f45097ae2635253aadd386bc7e34
abdulkareemmomin <abdulkareemmmomin275@gmail.com>
2026-02-27 22:18:04 +0530

    un-used code removed

--- src/main/java/com/revshop/controller/AuthController.java
+++ src/main/java/com/revshop/controller/AuthController.java
@@ -1,38 +1,3 @@
-//package com.revshop.controller;
-//
-//import com.revshop.entity.User;
-//import com.revshop.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//@Controller
-//public class AuthController {
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    // Show register page (Thymeleaf later)
-//    @GetMapping("/register")
-//    public String showRegisterPage() {
-//        return "register";
-//    }
-//
-//    // Handle Buyer/Seller Registration
-//    @PostMapping("/register")
-//    public String registerUser(@ModelAttribute User user) {
-//
-//        userService.registerUser(user); // 🔥 password encoded inside service
-//
-//        return "redirect:/login";
-//    }
-//
-//    // Custom login page mapping
-//    @GetMapping("/login")
-//    public String loginPage() {
-//        return "login";
-//    }
-//}
 package com.revshop.controller;
 
 import com.revshop.entity.User;
